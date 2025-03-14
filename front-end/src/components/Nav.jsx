2bbef67e66a0f85526eca5767d530fbb66c11af8
mekbalidris <mekbalidriss@gmail.com>
2025-03-14 07:08:29 +0100

    finished first half of the home page

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
         <nav>
             <header className='flex flex-row px-20 justify-between items-center'>
                 <div className='text-4xl text-white'>☰</div>
-                <div className='text-3xl font-main text-main drop-shadow-lg'>Chatbot</div>
+                <div className='text-4xl font-main text-main drop-shadow-lg'>Chatbot</div>
                 <img className='pt-2' src={profile} width={60} />
             </header>
         </nav>
