f51116763869a7536da38e4fab1e819c0e505f6a
mekbalidris <mekbalidriss@gmail.com>
2025-03-14 17:20:44 +0100

    fixed thing

--- front-end/src/components/Background.jsx
+++ front-end/src/components/Background.jsx
@@ -21,8 +21,7 @@ export default function Background() {
           </div>
         </div>
 
-        {/* Second Background */}
-        <div className="h-[100vh] bg-linear-to-t from-[#5B209A] to-[#36135A] z-[-1] rotate-180">
+        <div className="h-[100vh] bg-linear-to-t from-[#5B209A] to-[#36135A] z-[-1] rotate-180 ">
           <div className="absolute left-0 w-screen h-[60vh] overflow-hidden">
             <img
               className="absolute w-full h-[100vh] object-cover animate-[scrollStars_40s_linear_infinite] blur-[2px]"
