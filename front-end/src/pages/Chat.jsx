fc8619b2b738ea57f59660a7c4e1fc7b2f6f3d51
Youcef <abdelhakim1cef@gmail.com>
2025-03-17 15:52:08 -0700

    last changes chat page

--- front-end/src/pages/Chat.jsx
+++ front-end/src/pages/Chat.jsx
@@ -7,7 +7,7 @@ const ChatPage = () => {
     const [messages, setMessages] = useState([]);
     const [input, setInput] = useState("");
     const messagesEndRef = useRef(null);
-    const [chatToolsDisplay, setChatToolsDisplay] = useState("true");
+    const [chatToolsDisplay, setChatToolsDisplay] = useState(false);
 
     useEffect(() => {
         messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -84,49 +84,67 @@ const ChatPage = () => {
                 </div>
             </div>
 
-            <div className="fixed bottom-4 flex flex-row items-center px-4 w-full">
-                <div className="flex items-center w-full  bg-main rounded-full p-2 mr-5">
-                    {!chatToolsDisplay ? (
-                        <button className="bg-[#36135a] text-2xl font-bold text-white w-12 h-12 rounded-full flex items-center text-center justify-center flex-shrink-0"
-                            onClick={() => { setChatToolsDisplay(!chatToolsDisplay) }}>
-                            <i className='bx bx-plus'></i>
-                        </button>
-                    ) : (
-                        <div className="w-auto p-2 rounded-full bg-[#36135a] h-12 flex flex-row items-center gap-2">
-                            <button className="w-10 h-10 flex justify-center items-center rounded-full border border-white bg-[#CAB3E8]"
-                                onClick={() => { setChatToolsDisplay(!chatToolsDisplay) }}>
-                                <i className='bx bx-x text-[#36135a] text-xl'></i>
-                            </button>
-                            <button className="w-10 h-10 flex justify-center items-center rounded-full border border-white bg-[#CAB3E8]"
-                                onClick={defaultMessageFile}>
-                                <i className='bx bxs-file-blank text-[#36135a] text-xl'></i>
-                            </button>
-                            <button className="w-10 h-10 flex justify-center items-center rounded-full border border-white bg-[#CAB3E8]"
-                                onClick={defaultMessageFile}>
-                                <i className='bx bx-microphone text-[#36135a] text-xl'></i>
-                            </button>
-                        </div>
-
-                    )}
+            <div className="fixed bottom-4 flex flex-row items-center justify-center px-4 w-full md:px-0">
+                <div className="w-full flex flex-col md:w-[60%] bg-main rounded-4xl p-2 mr-5 ">
 
                     <textarea placeholder="Poser une question ..."
                         ref={textareaRef}
-                        className="w-full border-none px-4 py-2 flex-grow resize-none outline-none bg-transparent h-auto min-h-[40px] custom-scrollbar mr-1"
+                        className="mb-5 w-full border-none px-4 py-2 flex-grow resize-none outline-none bg-transparent h-auto min-h-[40px] custom-scrollbar "
                         rows="1"
                         onChange={handleInput}>
 
                     </textarea>
+                    <div className="flex flex-row relative">
 
-                    <button className="text-2xl border border-[#36135a] font-extrabold text-[#36135a] w-12 h-12 rounded-full flex items-center justify-center flex-shrink-0"
-                        onClick={defaultMessageFile}>
-                        <i className='bx bx-camera'></i>
-                    </button>
+                        <div className="flex flex-row flex-[4]">
+                            {!chatToolsDisplay ? (
+                                <button className="mr-3 bg-[#36135a] text-2xl font-bold text-white w-12 h-12 rounded-full flex items-center text-center justify-center flex-shrink-0"
+                                    onClick={() => { setChatToolsDisplay(!chatToolsDisplay) }}>
+                                    <i className='bx bx-plus'></i>
+                                </button>
+                            ) : (
+                                <div className="mr-3 w-auto p-2 rounded-full bg-[#36135a] h-12 flex flex-row items-center gap-2">
+                                    <button className="w-10 h-10 flex justify-center items-center rounded-full border border-white bg-[#CAB3E8]"
+                                        onClick={() => { setChatToolsDisplay(!chatToolsDisplay) }}>
+                                        <i className='bx bx-x text-[#36135a] text-xl'></i>
+                                    </button>
+                                    <button className="w-10 h-10 flex justify-center items-center rounded-full border border-white bg-[#CAB3E8]"
+                                        onClick={defaultMessageFile}>
+                                        <i className='bx bxs-file-blank text-[#36135a] text-xl'></i>
+                                    </button>
+                                    <button className="w-10 h-10 flex justify-center items-center rounded-full border border-white bg-[#CAB3E8]"
+                                        onClick={defaultMessageFile}>
+                                        <i className='bx bx-microphone text-[#36135a] text-xl'></i>
+                                    </button>
+                                </div>
+
+                            )}
+                            {
+                                chatToolsDisplay && (
+                                    <>
+                                        <button className="mr-3 font-bold border border-[#36135a] bg-main text-[#36135a] w-12 md:w-30 h-12 rounded-full flex items-center justify-center flex-shrink-0"
+                                            onClick={defaultMessageFile}>
+                                            <i class='bx bx-globe'></i> <span className="hidden md:flex">Search</span>
+                                        </button>
+                                        <button className="mr-3 font-bold border border-[#36135a] bg-main text-[#36135a] w-12 md:w-30 h-12 rounded-full flex items-center justify-center flex-shrink-0"
+                                            onClick={defaultMessageFile}>
+                                            <i class='bx bx-sun'></i> <span className="hidden md:flex">Think</span>
+                                        </button>
+                                    </>
+                                )
+                            }
+                        </div>
+
+
+
+
+                        <button className="text-2xl border border-[#36135a] bg-main font-extrabold text-[#36135a] w-12 h-12 rounded-full flex items-center justify-center flex-shrink-0 "
+                            onClick={sendMessage}>
+                            <i className='bx bx-up-arrow-alt'></i>
+                        </button>
+                    </div>
 
                 </div>
-                <button className="text-2xl border border-[#36135a] bg-main font-extrabold text-[#36135a] w-14 h-14 rounded-full flex items-center justify-center flex-shrink-0 "
-                    onClick={sendMessage}>
-                    <i className='bx bx-up-arrow-alt'></i>
-                </button>
             </div>
         </section>
     )
